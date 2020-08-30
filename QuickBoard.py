d156942c949a1b78fa5bd46d4915b2339cd2d36d
Xaler1 <32718751+Xaler1@users.noreply.github.com>
2020-08-30 14:26:47 +0100

    Add files via upload

--- QuickBoard.py
+++ QuickBoard.py
@@ -27,7 +27,7 @@ class QuickBoard(tk.Tk):
         frame.tkraise()
 
     def OpenYoutube(self):
-        os.system("sudo xdg-open https://www.youtube.com")
+        os.system("sudo chromium-browser https://www.youtube.com")
 
     def Shutdown(self):
         self.show_frame("ShutdownPage")
